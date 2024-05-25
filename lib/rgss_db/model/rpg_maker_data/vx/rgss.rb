52327c79d857fc3dc5503cda910adccf881f4a62
Fernando <fergarpaal@gmail.com>
2024-05-25 14:36:11 +0200

    feat: [vx-support] added model files

---
+++ lib/rgss_db/model/rpg_maker_data/vx/rgss.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Application mixins
+require_relative "../../mixins/jsonable"
+require_relative "../../mixins/jsonable_constructor"
+
+# RGSS Impl.
+require_relative "./rgss/color"
+require_relative "./rgss/rect"
+require_relative "./rgss/table"
+require_relative "./rgss/tone"
