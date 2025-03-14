1ca38698f3d8aab9c843163dccfdeb05ab682f94
Fernando <fergarpaal@gmail.com>
2025-03-14 20:53:48 +0100

    refactor: maps uses the new data file in factory

--- lib/rgss_db/model/data_file_factory.rb
+++ lib/rgss_db/model/data_file_factory.rb
@@ -68,11 +68,21 @@ module RgssDb
       raise "could not find a valid data file type for the file: '#{data_file}'" if type.nil?
 
       # Checks for a specific data file usage (bulk-check)
-      return DataFileArray.new(type, data_file, object) if FACTORY_ARRAY.any? { |f| f.casecmp?(type) }
-      return DataFileHash.new(type, data_file, object) if FACTORY_HASH.any? { |f| f.casecmp?(type) }
-      return DataFileHashNumber.new(type, data_file, object) if FACTORY_HASH_NUMBER.any? { |f| f.casecmp?(type) }
+      if FACTORY_ARRAY.any? { |f| File.fnmatch(f, type, File::FNM_CASEFOLD) }
+        # Object should be treated using a data file for arrays
+        return DataFileArray.new(type, data_file, object)
+      elsif FACTORY_HASH.any? { |f| File.fnmatch(f, type, File::FNM_CASEFOLD) }
+        # Object should be treated using a data file for hashes
+        return DataFileHash.new(type, data_file, object)
+      elsif FACTORY_HASH_NUMBER.any? { |f| File.fnmatch(f, type, File::FNM_CASEFOLD) }
+        # Object should be treated using a data file for hashes (forcing keys to numbers)
+        return DataFileHashNumber.new(type, data_file, object)
+      elsif File.fnmatch(DATA_FILE_MAPS, type, File::FNM_CASEFOLD)
+        # Object is a RPG Maker RPG::Map instance and should be treated in a special way
+        return DataFileMap.new(type, data_file, object)
+      end
 
-      # Assume a base data file (map data files will use this)
+      # Assume a base data file
       DataFile.new(type, data_file, object)
     end
 
